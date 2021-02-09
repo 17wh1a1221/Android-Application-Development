741758433baf89afe3208c3a37f16e8bb3b9d6e0
17wh1a1221 <57661295+17wh1a1221@users.noreply.github.com>
2021-02-09 17:26:45 +0530

    Add files via upload

---
+++ Lab program_5/Main2Activity.java
@@ -0,0 +1,12 @@
+package com.example.aad_5;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Main2Activity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
